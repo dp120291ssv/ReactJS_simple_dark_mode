0239ba66d315c63756f4f508fc93348cca0b38cf
Sergei Semenov <dp120291ssv@gmail.com>
2022-08-09 18:40:28 +0300

    add project

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, {useCallback, useState} from "react";
+import {ThemeProvider} from "styled-components";
+import lightTheme from "../themes/light";
+import darkTheme from "../themes/dark";
+import Container from "./Container";
+import Card from "./Card";
+import Button from "./Button";
+
+const App = () => {
+    const stored = localStorage.getItem("isDarkMode");
+    const [isDarkMode, setIsDarkMode] = useState(stored === "true");
+    const handleSwitchTheme = useCallback(() => {
+        setIsDarkMode(!isDarkMode);
+        localStorage.setItem("isDarkMode", !isDarkMode);
+    }, [isDarkMode])
+
+    return (
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+            <Container>
+                <Card>
+                    Dark Mode is Disabled
+                    <Button
+                        onClick={handleSwitchTheme}
+                    >Toggle Dark Mode</Button>
+                </Card>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default App;
